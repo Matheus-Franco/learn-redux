b687b8f91a1fbbb0862abfb3e430c0bda0ddb095
Matheus <matheusfrancoalves@hotmail.com>
2020-09-22 14:33:59 -0300

    use Immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from 'redux';
+import produce from 'immer';
 import { ICartState } from './types';
 
 const INITIAL_STATE: ICartState = {
@@ -6,26 +7,31 @@ const INITIAL_STATE: ICartState = {
 };
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload;
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
 
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
+        const productInCartIndex = draft.items.findIndex(
+          item => item.product.id === product.id,
+        );
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity += 1;
+        } else {
+          draft.items.push({
             product,
             quantity: 1,
-          },
-        ],
-      };
-    }
+          });
+        }
 
-    default: {
-      return state;
+        break;
+      }
+      default: {
+        return draft;
+      }
     }
-  }
+  });
 };
 
 export default cart;
