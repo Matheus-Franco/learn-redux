e099ddea4919e6c5c40beaa389701a34499742a0
Matheus <matheusfrancoalves@hotmail.com>
2020-09-22 13:44:37 -0300

    add linting

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -3,35 +3,28 @@ import api from '../services/api';
 import { IProduct } from '../store/modules/cart/types';
 
 const Catalog: React.FC = () => {
-    const [catalog, setCatalog] = useState<IProduct[]>([]);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-    useEffect(() => {
-        api.get('products').then(response => {
-            setCatalog(response.data);
-        });
-    }, []);
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data);
+    });
+  }, []);
 
-    return (
-        <main>
-            <h1>Catalog Component</h1>
-            {
-                catalog.map(product => (
-                    <article key={product.id}>
-                        <strong>
-                            {product.title}
-                        </strong> { "-" }
-                        <span>
-                            {product.price}
-                        </span> { "-" }
-
-                        <button type="button">
-                            Comprar
-                        </button>
-                    </article>
-                ))
-            }
-        </main>
-    );
-}
+  return (
+    <main>
+      <h1>Catalog Component</h1>
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong>
+          {' - '}
+          <span>{product.price}</span>
+          {' - '}
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  );
+};
 
 export default Catalog;
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -2,11 +2,11 @@ import { Reducer } from 'redux';
 import { ICartState } from './types';
 
 const INITIAL_STATE: ICartState = {
-    items: []
-}
+  items: [],
+};
 
 const cart: Reducer<ICartState> = () => {
-    return INITIAL_STATE;
-}
+  return INITIAL_STATE;
+};
 
-export default cart; 
+export default cart;
